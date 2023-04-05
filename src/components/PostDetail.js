da473ce50903dda5ff0d12fc9059d148804c730c
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-04-06 00:01:21 +0530

    added postDetail func

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import { firestore } from "../firebase";
+import { useParams } from "react-router-dom";
+
 function PostDetail(){
+    const [post, setPost] = useState({});
+    let { postId } = useParams();
+
+    useEffect(() => {
+        firestore.collection('posts').doc(postId).get().then((snapshot) => {
+            setPost(snapshot.data());
+        })
+    }, []);
+
     return (
-        <div className="">
-            PostDetail
+        <div className="post-detail">
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
         </div>
     );
 }
