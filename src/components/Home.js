e5ff68f3399dbe09fe4d6a6ddfbfc8ccb60558bb
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-04-05 18:24:48 +0530

    added home function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { firestore } from "../firebase";
+
 function Home(){
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        firestore.collection('posts').onSnapshot((snapshot) => {
+            const posts = snapshot.docs.map((doc) => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
+                }
+            });
+            setPosts(posts);
+        });
+    }, []);
+
     return (
-        <div className="">
-            Home
+        <div className="home">
+            <h1>React Blog using Hooks</h1>
+            <div id="blog-by">Arjun</div>
+            {posts.map((post, index) => (
+                <div className="post" key={`post-${index}`}>
+                    <Link to={`/post/${post.id}`}>
+                        <h3>{post.title}</h3>
+                    </Link>
+                    <p>{post.subTitle}</p>
+                </div>
+            ))}
         </div>
     );
 }
